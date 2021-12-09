4aa6e0cbe6137c045408abb4f12fb1e57cd695e3
torpu <49022023+torpu@users.noreply.github.com>
2021-12-09 15:54:18 +0000

    Update providers.tf

--- env/providers.tf
+++ env/providers.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription-id
   version         = "~> 2.46.0"
-  client_id       = var.client-id
-  client_secret   = var.client-secret
+  #client_id       = var.client-id
+  #client_secret   = var.client-secret
   tenant_id       = var.tenant-id
 }
